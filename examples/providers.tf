bb8ffcfe62af73aba39eedde442a8c0926a48e36
MonsieurDa <41913527+MonsieurDa@users.noreply.github.com>
2022-01-28 15:11:56 +0100

    Update providers.tf

--- examples/providers.tf
+++ examples/providers.tf
@@ -1,8 +1,8 @@
 terraform {
   required_version = ">= 0.13.1" # see https://releases.hashicorp.com/terraform/
   backend "gcs" {
-    bucket = "cloudorbit"
-    prefix = "main"
+    bucket = "gcp-deploy-terraform"
+    #prefix = "main"
   }
   required_providers {
     google = {
